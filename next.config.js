f5c8a953f3e1d913e1f28152d03205ecaf598b73
Sam <sam.kaniampuram@gmail.com>
2022-12-27 02:50:54 +0100

    try pwa again

--- next.config.js
+++ next.config.js
@@ -1,17 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
-
-module.exports = nextConfig
-// const withPWA = require('next-pwa')({
-//   dest: 'public'
-// })
-
-// module.exports = withPWA({
-//   // next.js config
+// const nextConfig = {
 //   reactStrictMode: true,
 //   swcMinify: true,
-//   images: {unoptimized: true},
-// })
+// }
+
+// module.exports = nextConfig
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
+
+module.exports = withPWA({
+  // next.js config
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {unoptimized: true},
+})
